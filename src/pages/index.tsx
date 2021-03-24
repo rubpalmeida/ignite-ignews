4bef88ab18b9dcff6471b040ab93ff0e5b454cde
Rubens Almeida <rubpalmeida@gmail.com>
2021-03-23 22:55:29 -0300

    Header Component added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,14 +2,8 @@ import Head from 'next/head'
 
 export default function Home() {
   return (
-    <>
-      <Head>
-        <title>Home | ig.news</title>
-      </Head>
-
-      <h1>
-        Hello World
-      </h1>
-    </>
+    <Head>
+      <title>Home | ig.news</title>
+    </Head>
   )
 }
