ef86729592c9dde8f1b3f18bd34b4a86040668f3
Rubens Almeida <rubpalmeida@gmail.com>
2021-03-23 22:24:06 -0300

    Application Structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,15 @@
+import Head from 'next/head'
+
 export default function Home() {
   return (
-    <h1>Hello World</h1>
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+
+      <h1>
+        Hello World
+      </h1>
+    </>
   )
 }
