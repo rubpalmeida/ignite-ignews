5b016974870034e637623b2a9f636ead15f768ce
Rubens Almeida <rubpalmeida@gmail.com>
2021-05-19 11:40:17 -0300

    Link updated

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,28 @@
 import React from 'react';
+
+import Link from 'next/link'
+
+import { useRouter } from 'next/router';
+
 import { SignInButton } from '../SignInButton';
+
 import styles from './styles.module.scss';
 
 export function Header() {
+  const { asPath } = useRouter()
+
+  console.log(asPath)
   return (
     <header className={styles.headerContainer}>
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <Link href="/">
+            <a className={styles.active}>Home</a>
+          </Link>
+          <Link href="/posts">
+            <a>Posts</a>
+          </Link>
         </nav>
         <SignInButton />
       </div>
